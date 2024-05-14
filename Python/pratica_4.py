1b4448a163b2e6b6bcc71ca04898ced060542b27
Allexandre484 <168091567+Allexandre484@users.noreply.github.com>
2024-05-14 12:23:25 -0300

    Update pratica_4.py

--- Python/pratica_4.py
+++ Python/pratica_4.py
@@ -44,7 +44,50 @@ for i in range(num1,num2+1):
 print("\nEntre o intervalo dos números",num1,"e",num2,"existem",par,"números pares, e",impar,"números ímpares.")
 
 
-#Exercicio 5(incompleta): Solicite um número inteiro positivo N ao usuário e calcule a soma dos primeiros N números naturais.
+#Exercicio 5: Solicite um número inteiro positivo N ao usuário e calcule a soma dos primeiros N números naturais.
 num=int(input("Insira um número positivo e inteiro: "))
-soma = 0
-print("A soma dos números naturais até {num} são equivalentes à: {soma}".format(num,soma))
+num2 = num
+while num2 >0:
+    num2-=1
+    num=num+num2
+print("A soma dos números naturais até este número é equivalente à: {}".format(num))
+
+
+#Exercicio 6: Solicite ao usuário a quantidade de notas que ele deseja inserir, em seguida, solicite essas notas e calcule a média.
+qnt=int(input("Insira o número equivalente à quantidade de notas que você deseja descobrir a média: "))
+media=0
+print('')
+for qnt in range(1,qnt+1):
+    nota=int(input("Insira a sua nota: "))
+    media +=nota
+print("A sua média é equivalente à:",media/qnt)
+
+
+#Exercicio 7: Solicite um número inteiro positivo ao usuário e calcule o fatorial desse número.
+num=int(input("Insira um número inteiro e positivo: "))
+soma = 1
+for i in range(1,num+1):
+    soma*=i
+print("O valor fatorial do número",num,"! é equivalente à:",soma)
+
+
+#Exercicio 8: Desenvolva um programa em Python que permita ao usuário escolher um número e gere a tabuada desse número até um limite especificado pelo próprio usuário.
+num=int(input("Insira o número correspondente a tabuada desejada: "))
+num2=int(input("Até que número você deseja ver a tabuada: "))
+valor=1
+
+for i in range(1,num2+1):
+    print(num,"X",valor,"=",(num*valor))
+    valor+=1
+
+
+
+#Exercicio 9: Desenvolva um programa em Python que solicite ao usuário um número inteiro. O programa deverá então exibir a tabuada de multiplicação até o número escolhido pelo usuário, fazendo as multiplicações de 1 até 10.
+num=int(input("Insira o número correspondente à até que tabuada você deseja ver: "))
+for i in range(1,num+1):
+    print('')
+    for y in range(1,11):
+       print('{} x {} = {}'.format(i,y,i*y))
+
+   
+   
